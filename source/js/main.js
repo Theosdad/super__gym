5258ca3be695fcd8088434c8bc365f124a8f296e
Theosdad <Obie_1@mail.ru>
2023-04-09 16:35:38 +0300

    almost swiper, slaves

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {Form} from './modules/form-validate/form';
 import './modules/video';
 import './modules/tab';
+import './modules/init-swiper';
 
 // ---------------------------------
 
