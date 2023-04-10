06de7e0172c04193fad4e8dc7ca0a5d6190b7816
Theosdad <Obie_1@mail.ru>
2023-04-10 04:20:16 +0300

    swiper, slaves

--- source/js/modules/init-swiper.js
+++ source/js/modules/init-swiper.js
@@ -1,11 +1,15 @@
-const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: true,
-    
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    slidesPerView: 4,
-  });
-  
+const initSwipers = () => {
+    const slider = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: true,
+
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        slidesPerView: 4,
+    });
+}
+
+initSwipers();
